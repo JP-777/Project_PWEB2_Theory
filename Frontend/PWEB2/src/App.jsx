4736cb25e63ae388c211123c18e39d491bfb3774
KevinCallo <168785875+KevinCallo@users.noreply.github.com>
2024-12-16 20:23:41 -0500

    Add files via upload

--- Frontend/PWEB2/src/App.jsx
+++ Frontend/PWEB2/src/App.jsx
@@ -1,80 +1,19 @@
+import React from 'react';
 import './App.css';
-import { NavBar } from './components/NavBar.jsx';
-import { LoginForm } from './components/LoginForm';
-import { RegisterForm } from './components/RegisterForm';
-import { AccessBar } from './components/AccessBar.jsx';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import Precios from './Componentes/Precios.jsx';
+import TrivagoFeatures from './Componentes/TrivagoFeatures';
 
-export function App() {
-
-    useEffect(() => {
-        localStorage.removeItem('token');
-        setIsAuthenticated(false);
-    }, []);
-
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            try {
-                const payload = JSON.parse(atob(token.split('.')[1]));
-                const isExpired = payload.exp * 1000 < Date.now();
-                if (!isExpired) {
-                    setIsAuthenticated(true);
-                } else {
-                    console.warn('El token ha expirado');
-                    localStorage.removeItem('token');
-                }
-            } catch (error) {
-                console.error('Token inválido:', error);
-                localStorage.removeItem('token');
-            }
-        }
-    }, []);
-    
-    const handleLoginSuccess = () => {
-        setIsAuthenticated(true);
-    };
-
-    return (
-        <Router>
-            <div className="App">
-                {isAuthenticated && (
-                    <nav>
-                        <NavBar
-                            selfProfilePhoto={`https://unavatar.io/JP-777`}
-                            selfProfileName="JP-777"
-                        />
-                    </nav>
-                )}
-                <div className="principalBody">
-                    <Routes>
-                        <Route path="/" element={isAuthenticated ? (
-                                    <>
-                                      <AccessBar/>
-                                      <PostingPanel
-                                        content={[
-                                          { id: 1, userName: "Saul Andre Sivincha Machaca" },
-                                          { id: 2, userName: "Matias Dario Davila Flores" },
-                                          { id: 3, userName: "Jefferson Joao Basurco Cassani" },
-                                        ]}
-                                      />
-                                      <FriendBar />
-                                
-                                    </>
-                                ) : (
-                                  <Navigate to="/login" />
-                                )
-                            }
-                        />
-                        <Route path="/login" element={isAuthenticated ? (<Navigate to="/" />) : (<LoginForm onLoginSuccess={handleLoginSuccess} />)}/>
-                        <Route path="*" element={<Navigate to={isAuthenticated ? "/" : "/login"} />} />
-                        <Route path="/register" element={<RegisterForm />} />
-                    </Routes>
-                </div>
-            </div>
-        </Router>
-    );
+function App() {
+  return (
+    <div className="App">
+      <div className="background-container">
+        <h1 className="titulo">Descubre el mejor momento para reservar</h1>
+        <Precios />
+      </div>
+      <TrivagoFeatures />
+    </div>
+  );
 }
+
+export default App;
+
